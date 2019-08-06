34e3d7d6343637d37522d47284f82e25adc59298
oliviaalchemy <olivia@alchemytechs.com>
2019-08-06 14:43:13 +0100

    Add project files.

---
+++ Building a Mad Libs Game/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Building_a_Mad_Libs_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string colour, pluralNoun, celebrity;
+
+            Console.Write("Enter a colour: ");
+            colour = Console.ReadLine();
+
+            Console.Write("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are " + colour);
+            Console.WriteLine(pluralNoun + " are blue");
+            Console.WriteLine("I Love " + celebrity);
+
+            Console.ReadLine();
+        }
+    }
+}
